56ac0e2054ee64fed8d9e7b8bdd3a6d60e020377
CristianPietro <crpietro26@gmail.com>
2022-10-18 10:08:59 -0300

    aula de expressões e operadores concluida

---
+++ Guia Estelar Javascript/expressões e operadores/main.js
@@ -0,0 +1,24 @@
+let number = 1
+//console.log(++number)
+//console.log(number + 10)
+//console.log(true ? "Hello, World" : "nada")
+
+
+//
+
+let newString = new String("Pietro")
+console.log(newString)
+
+let newDate = new Date('2020-12-01')
+console.log(newDate)
+
+//
+
+let age = 17;
+const canDriver = age >= 18 ? 'can drive' : 'cannot drive'
+
+console.log(canDriver)
+
+//
+
+console.log( NaN ? 'true' : 'false')
---
+++ Guia Estelar Javascript/expressões e operadores/texto.md
@@ -0,0 +1,20 @@
+# Expressões e operadores
+
+- Expression
+- Operators
+  Binary
+  Unary
+  Ternary
+
+  //
+
+
+  # New
+
+  *Left-hand-side expression*
+
+  *Criar um novo objeto*
+
+  //
+
+   
